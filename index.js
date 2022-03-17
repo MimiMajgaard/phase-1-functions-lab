859021a425c086bcb14d5e6066c64fcc563723a4
Mimi Majgaard <mimimajgaard@gmail.com>
2022-03-17 16:55:56 -0700

    commit tree 996629e42c90826fd3c307ff03899cc5263a4666

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num1){
+    
+    if (num1 < 42) {
+    return 42-num1
+    }
+    else {return num1-42}
+}
+
+
+function distanceFromHqInFeet(num2){
+    
+     return distanceFromHqInBlocks(num2) * 264
+}
+
+function distanceTravelledInFeet(a, b){
+    if (a > b) {
+    return (a-b) * 264 
+    }
+    else {return (b-a)*264}
+}
+
+function calculatesFarePrice(a, b){
+    let distance = distanceTravelledInFeet(a, b)
+    if (distance <= 400){
+    return 0    
+} else if (distance > 400 && distance <= 2000){
+return 0.02 * (distance - 400)
+}
+else if (distance > 2000 && distance < 2500){
+    return 25
+}
+else {
+   return 'cannot travel that far' 
+}
+ }
+    
